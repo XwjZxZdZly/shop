8aa1469885636764d46636d2f4e6fc54d962a794
ikarose <ikarosexu@gmail.com>
2017-12-06 21:08:23 +0800

    router1.1 zx

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+
+ 
 
   constructor() { }
 
