d67278997c0ac31d4f0975fa009745dc7b29cd58
xxxx <740240490@qq.com>
2017-12-06 20:40:16 +0800

    product service commit by sean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import {HttpModule} from "@angular/http";
-
 import {Routes,RouterModule} from "@angular/router";
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
