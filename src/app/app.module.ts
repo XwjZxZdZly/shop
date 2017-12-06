5c78db886d4c4f5311b050780d1fe373532a4cad
ikarose <ikarosexu@gmail.com>
2017-12-06 19:35:59 +0800

    主路由已创立

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {HttpModule} from "@angular/http";
 
+import {Routes,RouterModule} from "@angular/router";
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BuyComponent } from './buy/buy.component';
+import { SaleComponent } from './sale/sale.component';
+import { MineComponent } from './mine/mine.component';
 
+const routes:Routes = [
+  {path:"",component:IndexComponent},
+  {path:"home",component:ProductDetailComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BuyComponent,
+    SaleComponent,
+    MineComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
