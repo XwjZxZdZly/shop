8fa11b12064b5f5286ea111e575d3139d5844b23
callmezd <476182045@qq.com>
2017-12-07 14:06:16 +0800

    zd

--- src/app/cate/cate.component.ts
+++ src/app/cate/cate.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cate',
   templateUrl: './cate.component.html',
-  styleUrls: ['./cate.component.css']
+  styleUrls: ['./cate.component.scss']
 })
 export class CateComponent implements OnInit {
 
