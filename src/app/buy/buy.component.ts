515e4725a28b208d5951a8bf890a77e018f8ecd1
callmezd <476182045@qq.com>
2017-12-07 21:25:05 +0800

    分类页和淘二手页 zd

--- src/app/buy/buy.component.ts
+++ src/app/buy/buy.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-buy',
   templateUrl: './buy.component.html',
-  styleUrls: ['./buy.component.css']
+  styleUrls: ['./buy.component.scss']
 })
 export class BuyComponent implements OnInit {
 
