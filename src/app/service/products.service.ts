e7cc79b44f4879090311c1bfedb8e84a82c1fa49
xxxx <740240490@qq.com>
2017-12-12 08:42:57 +0800

    hhhhh

--- src/app/service/products.service.ts
+++ src/app/service/products.service.ts
@@ -6,3 +6,12 @@ export class ProductsService {
   constructor() { }
 
 }
+
+export class Product{
+  constructor(
+    public id:number,
+    public title:string,
+    public price:number,
+    public num:number
+  ){}
+}
