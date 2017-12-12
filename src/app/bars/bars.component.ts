4b942660c059aa52a79719c56069cdf7ef8ae015
Ginx <441420429@qq.com>
2017-12-13 00:54:07 +0800

    bars

--- src/app/bars/bars.component.ts
+++ src/app/bars/bars.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit  ,Input} from '@angular/core';
 
 @Component({
   selector: 'app-bars',
   templateUrl: './bars.component.html',
-  styleUrls: ['./bars.component.css']
+  styleUrls: ['./bars.component.scss']
 })
 export class BarsComponent implements OnInit {
+@Input()
+cate:Array<Cate>
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.cate)
   }
 
+}
+
+export class Cate{
+  constructor(
+    icon:string,
+    title:string,
+    path:string
+  ){}
 }
--- src/app/mine/mine.component.ts
+++ src/app/mine/mine.component.ts
@@ -1,3 +1,4 @@
+import { Cate } from './../bars/bars.component';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mine.component.scss']
 })
 export class MineComponent implements OnInit {
-
+  private cates:Array<Cate>=[
+    {
+      title:"我的订单",
+      icon:"\ue606"
+    },{
+      title:"我发布的",
+      icon:"\ue6f5"
+    },
+    {
+      title:"我的购物车",
+      icon:"\ue652;"
+    },
+    {
+      title:"我的消息",
+      icon:"\ue6ab"
+    },
+    {
+      title:"我的红包",
+      icon:"\ue784"
+    },
+  ]
   constructor() { }
-
+  
   ngOnInit() {
+    console.log(this.cates)
   }
 
 }
